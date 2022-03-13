db2b2d38705d93ab9af23c8e38cb741984f14da2
rck <296046734@qq.com>
2022-03-13 22:36:17 +0800

    fix

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: "浏览器",
+    base: '/browser/',
+    themeConfig: {
+      nav: [{
+        text: 'Home', link: '/'
+      }, {
+        text: 'Github', link: 'https://github.com/l1872427108/uui'
+      }, {
+        text: 'Npm', link: 'https://www.npmjs.com/package/uu-uui'
+      }],
+      sidebar: [
+        '/'
+      ]
+    }
+  }
+  
